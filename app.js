ffd6579b4b0d0863e205dffc58193155484c69b4
shriram-mnit <43758704+shriram-mnit@users.noreply.github.com>
2020-05-14 13:16:21 +0530

    add files

---
+++ app.js
@@ -0,0 +1,29 @@
+const express =require('express')
+const mongoose = require('mongoose')
+const {MONGOURI}= require('./keys')
+const cors= require('cors')
+const app =express()
+const PORT =5000
+
+app.use(cors())
+
+mongoose.connect(MONGOURI,{
+	 useNewUrlParser:true,
+	useUnifiedTopology:true
+})
+
+mongoose.connection.on('connected',()=>{
+	console.log("connected to mongo ")
+})
+mongoose.connection.on('error',(err)=>{
+	console.log("err connecting",err)
+})
+require('./models/user')
+
+app.use(express.json())
+
+app.use(require('./routes/auth'))
+
+
+
+app.listen(5000)
